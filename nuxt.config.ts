4dfaf958eec439cca663b6d58826045a56370a3c
mcursch <115659776+mcursch@users.noreply.github.com>
2024-10-05 15:17:42 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   ssr: false,
   app: {
-    baseURL: '/',
+    baseURL: '/portfolio_website/',
     buildAssetsDir: 'assets',
   },
   devtools: { enabled: true },

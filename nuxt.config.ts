e4772531fbf781c771c732195e0ea3f72b79bebd
Matt Curschman <mcurschman@conferencecatalysts.com>
2024-10-06 15:34:39 -0400

    another change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   target: 'static',
+  router: {
+    base: "/portfolio_website/"
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
